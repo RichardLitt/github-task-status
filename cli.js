176dea516d32d5f59f9a8c9e28d15a921aed44a3
Richard Littauer <richard.littauer@gmail.com>
2016-06-02 17:14:38 +0100

    Parse tasks and return percentage

--- cli.js
+++ cli.js
@@ -6,9 +6,9 @@ const githubTaskStatus = require('./')
 const Promise = require('bluebird')
 const ghauth = Promise.promisify(require('ghauth'))
 const authOptions = {
-  configName: 'ghIssueCommenters',
-  note: 'Get GitHub issue commenters',
-  userAgent: 'ghIssueCommenters',
+  configName: 'ghTaskStatus',
+  note: 'Get GitHub task status',
+  userAgent: 'ghTaskStatus',
   scope: ['repo']
 }
 
@@ -16,20 +16,18 @@ const cli = meow([`
   Usage
     $ github-task-status [input]
 
-  Options
-    --foo  Lorem ipsum. [Default: false]
-
   Examples
-    $ github-task-status
-    unicorns & rainbows
-    $ github-task-status ponies
-    ponies & rainbows
+    $ github-task-status https://api.github.com/repos/RichardLitt/github-task-status/issues/1
+    {done: 6, undone: 1}
 `, {
   alias: {}
 }])
 
-Promise.try(() => ghauth(authOptions)})
-.then((authData) => githubTaskStatus(cli.input[0], cli.flags, authData.token)})
-.map((response) => {
-  console.log(response)
-})
+// Options
+// --foo  Lorem ipsum. [Default: false]
+
+Promise.try(() => ghauth(authOptions))
+  .then((authData) => githubTaskStatus(cli.input[0], cli.flags, authData.token))
+  .then((response) => {
+    console.log(response)
+  })
--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 'use strict'
 
-module.exports = function (str, opts) {
-  if (typeof str !== 'string') {
-    throw new TypeError('Expected a string')
+const Octokat = require('octokat')
+var octo
+const Promise = require('bluebird')
+const moment = require('moment')
+const _ = require('lodash')
+// const depaginate = require('depaginate')
+// const getGithubUser = require('get-github-user')
+const emptyCheck = /[ ]*- \[ \] /g
+const filledCheck = /[ ]*- \[x\] /g
+
+var done = 0
+var undone = 0
+
+module.exports = function (org, opts, token) {
+  octo = new Octokat({
+    token: token || process.env.GITHUB_OGN_TOKEN
+  })
+
+  return Promise.resolve().then(() => octo.fromUrl(org).fetch())
+    .then((res) => res.body.split('\n'))
+    .map((body) => {
+      var checkEmpty = emptyCheck.exec(body)
+      if (checkEmpty && checkEmpty.index === 0) done += 1
+      var checkFileld = filledCheck.exec(body)
+      if (checkFileld && checkFileld.index === 0) undone += 1
+      }).then(() => {
+      return {done: done, undone: undone}
+    })
   }
-
-  opts = opts || {}
-
-  return str + ' & ' + (opts.postfix || 'rainbows')
-}
