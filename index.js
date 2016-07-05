636dd934607817d1acf1475eaee9d20e74757ecd
Richard Littauer <richard.littauer@gmail.com>
2016-07-05 12:50:27 +0100

    Enable for arrays

--- index.js
+++ index.js
@@ -9,43 +9,53 @@ const Promise = require('bluebird')
 const emptyCheck = /- \[ \]/
 const filledCheck = /- \[x\]/
 
-module.exports = function (org, opts, token) {
-  var done = 0
-  var undone = 0
+module.exports = function (url, opts, token) {
   octo = new Octokat({
     token: token || process.env.GITHUB_OGN_TOKEN
   })
 
-  return Promise.resolve(org)
-    .map((url) => {
-      // Prepend url to Owner/Repo
-      if (!url.match(/^https:\/\/github.com/) && !url.match(/^https:\/\/api.github.com\/repos/)) {
-        url = 'https://api.github.com/repos/' + url
-      }
-      // console.log('url', url)
-      // Replace Owner/Repo#1 with Owner/Repo/issues/1
-      url = url.replace(/#/, '/issues/')
-      // Prepend API text to normal URL
-      url = url.replace(/https:\/\/github.com/, 'https://api.github.com/repos')
-      return url
-    })
-    .then((res) => octo.fromUrl(res).fetch())
-    .then((res) => res.body.split('\n'))
-    .map((body) => {
-      var checkEmpty = emptyCheck.exec(body)
+  return Promise.each(url, (url) => {
+    // Prepend url to Owner/Repo
+    if (!url.match(/^https:\/\/github.com/) && !url.match(/^https:\/\/api.github.com\/repos/)) {
+      url = 'https://api.github.com/repos/' + url
+    }
+    // console.log('url', url)
+    // Replace Owner/Repo#1 with Owner/Repo/issues/1
+    url = url.replace(/#/, '/issues/')
+    // Prepend API text to normal URL
+    url = url.replace(/https:\/\/github.com/, 'https://api.github.com/repos')
+    return url
+  })
+  .map((res) => octo.fromUrl(res).fetch())
+  .map((res) => {
+    return {
+      url: res.url,
+      htmlUrl: res.htmlUrl,
+      body: res.body.split('\n')
+    }
+  })
+  .map((res) => {
+    var done = 0
+    var undone = 0
+
+    return Promise.each(res.body, (line) => {
+      var checkEmpty = emptyCheck.exec(line)
       if (checkEmpty && checkEmpty.index === 0) {
         undone += 1
       }
-      var checkFilled = filledCheck.exec(body)
+      var checkFilled = filledCheck.exec(line)
       if (checkFilled && checkFilled.index === 0) {
         done += 1
       }
     }).then(() => {
       return {
+        url: res.url,
+        htmlUrl: res.htmlUrl,
         done: done,
         undone: undone,
         progressio: 'http://progressed.io/bar/' + Math.round(done / (done + undone) * 100),
         progressioTitle: 'http://progressed.io/bar/' + Math.round(done / (done + undone) * 100) + `?title=${done}/${undone + done}`
       }
     })
+  })
 }
